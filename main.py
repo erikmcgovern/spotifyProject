35e11ad5acb5b29dfc8ace112194e4fd78556597
Erik <erikmcgov@gmail.com>
2024-08-15 09:37:25 -0400

    Test Commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import streamlit as st
+import pandas as pd
+import os
+
+CLIENT_ID = os.environ.get('CLIENT_ID')
+CLIENT_SECRET = os.environ.get('CLIENT_ID')
+REDIRECT_URI = 'http://localhost:5000'
+
+# sp = spotipy.Spotify(
+#     auth_manager=SpotifyOAuth(
+#         client_id=CLIENT_ID,
+#         client_secret=CLIENT_SECRET,
+#         redirect_uri=REDIRECT_URI,
+#         scope='user-top-read'
+#     )
+# )
+
+
+print('Hello')
+print('commit')
