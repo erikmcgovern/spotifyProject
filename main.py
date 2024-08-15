b18505f187f6ed7c6b8ac182f95b0f002dd69492
Erik <erikmcgov@gmail.com>
2024-08-15 09:49:54 -0400

    Test Commit

--- main.py
+++ main.py
@@ -8,15 +8,11 @@ CLIENT_ID = os.environ.get('CLIENT_ID')
 CLIENT_SECRET = os.environ.get('CLIENT_ID')
 REDIRECT_URI = 'http://localhost:5000'
 
-# sp = spotipy.Spotify(
-#     auth_manager=SpotifyOAuth(
-#         client_id=CLIENT_ID,
-#         client_secret=CLIENT_SECRET,
-#         redirect_uri=REDIRECT_URI,
-#         scope='user-top-read'
-#     )
-# )
-
-
-print('Hello')
-print('bye world')
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        redirect_uri=REDIRECT_URI,
+        scope='user-top-read'
+    )
+)
